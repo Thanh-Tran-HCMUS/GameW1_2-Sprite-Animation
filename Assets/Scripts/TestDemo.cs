c56932ab71de4127842b5c97f9de366c2a42cc7f
DatThanhTran <datthanh94@gmail.com>
2020-07-11 20:34:49 +0700

    Upload source code

---
+++ Assets/Scripts/TestDemo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDemo : MonoBehaviour
+{
+    //Script để demo về cách điều khiển animation cơ bản
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Luôn luôn lấy component trước khi thực hiện
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Cách 1: đặt parameter là trigger, xong gọi set trigger để play animation
+            anim.SetTrigger("TriggerAttack");
+
+            //Cách 2: gọi phương thức Play để play animation với tên gọi của state
+            //anim.Play("aw2");
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerCyclone.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCyclone : MonoBehaviour
+{
+    //Script demo cho việc xài animation event trong tab Animation
+
+    //Lấy thông tin game object cần điều khiển
+    public GameObject cyclone;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Lấy thành phần animator của game object cần điều khiển
+        anim = cyclone.GetComponent<Animator>();
+    }
+
+    //Cài đặt phương thức điều khiển bật animation trong animator qua biến trigger
+    public void SetCycloneTrigger()
+    {
+        anim.SetTrigger("Cyclone");
+    }
+
+    //Cài đặt phương thức dùng để hiển thị sprite khi chạy vào đúng event
+    public void VisibleObj()
+    {
+        cyclone.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    //Cài đặt phương thức dùng để tắt hiển thị sprite khi chạy xong event
+    public void InvisibleObj()
+    {
+        cyclone.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
